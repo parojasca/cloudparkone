ab78769020e41ec48f6bca8d7281694fc9723182
PedroR <pedro7277@gmail.com>
2019-03-28 22:42:38 -0500

    mapa parque

--- src/app/parque/parque.page.ts
+++ src/app/parque/parque.page.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { AlertController } from '@ionic/angular';
-import { ModalController } from '@ionic/angular';
+
+import { Component, OnInit, ElementRef, NgZone, ViewChild } from '@angular/core';
+import { AlertController,ModalController } from '@ionic/angular';
+
 import {AddArticlePage} from '../add-article/add-article.page'
+import {Geolocation} from '@ionic-native/geolocation/ngx';
+declare var google: any;
+
 @Component({
   selector: 'app-parque',
   templateUrl: './parque.page.html',
   styleUrls: ['./parque.page.scss'],
 })
+
 export class ParquePage implements OnInit {
+
+  @ViewChild('Map') mapElement: ElementRef;
+  map: any;
+  mapOptions: any;
+  location = {lat: null, lng: null};
+  markerOptions: any = {position: null, map: null, title: null};
+  marker: any;
+  apiKey: any = 'AIzaSyAjTjGIunMIKzq8jybWY-cm4d1N9-4-dGQ'; /*Your API Key*/
+
+
 //,public modalController: ModalController
-  constructor(public alertController: AlertController,public modalController: ModalController) { }
+  constructor(public alertController: AlertController,
+    public modalController: ModalController,
+    public zone: NgZone,
+    public geolocation: Geolocation) {
+  }
+     
 
   async ngOnInit() {
     const alert = await this.alertController.create({
@@ -19,6 +39,37 @@ export class ParquePage implements OnInit {
     });
 
     await alert.present();
+
+    
+      /*load google map script dynamically */
+      const script = document.createElement('script');
+      script.id = 'googleMap';
+      if (this.apiKey) {
+          script.src = 'https://maps.googleapis.com/maps/api/js?key=' + this.apiKey;
+      } else {
+          script.src = 'https://maps.googleapis.com/maps/api/js?key=';
+      }
+      document.head.appendChild(script);
+      /*Get Current location*/
+      this.geolocation.getCurrentPosition().then((position) =>  {
+          this.location.lat = position.coords.latitude;
+          this.location.lng = position.coords.longitude;
+      });
+      /*Map options*/
+      this.mapOptions = {
+          center: this.location,
+          zoom: 19,
+          mapTypeControl: false,
+          fullscreenControl: false
+      };
+      setTimeout(() => {
+          this.map = new google.maps.Map(this.mapElement.nativeElement, this.mapOptions);
+          /*Marker Options*/
+          this.markerOptions.position = this.location;
+          this.markerOptions.map = this.map;
+          this.markerOptions.title = 'My Location';
+          this.marker = new google.maps.Marker(this.markerOptions);
+      }, 3000);
   }
 
   async presentModal() {
@@ -60,4 +111,6 @@ export class ParquePage implements OnInit {
       
     },
   ]
+
+  
 }
